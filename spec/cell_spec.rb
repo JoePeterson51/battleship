f252f6e3b46d676fa1ef04abca2d32e0479592b0
JoePeterson51 <>
2021-03-29 18:01:04 -0600

    Create test for multi cell board

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -122,4 +122,27 @@ describe Cell do
         end 
     end 
 
+    describe 'multi cell edge case' do 
+        it 'can work with multi cells' do 
+            cell_1 = Cell.new("B4")
+            cell_2 = Cell.new("B5")
+            cell_3 = Cell.new("B6")
+            cruiser = Ship.new("Cruiser", 2)
+            cell_1.place_ship(cruiser)
+            cell_2.place_ship(cruiser)
+            cell_3.fire_upon
+
+            expect(cell_3.render).to eq("M")
+            expect(cell_2.render(true)).to eq("S")
+            expect(cell_1.render).to eq(".")
+
+            cell_1.fire_upon
+            expect(cell_1.render).to eq("H")
+
+            cell_2.fire_upon
+            expect(cell_1.render).to eq("X")
+            expect(cell_2.render).to eq("X")
+        end 
+    end 
+
 end 
