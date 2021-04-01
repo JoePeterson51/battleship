48f8d23f15ca27115add9f1d313b3a9bb059de04
NoahZinter <noah.zinter@gmail.com>
2021-04-01 14:16:30 -0600

    Refactor initialize to auto-populate @cells

--- lib/board.rb
+++ lib/board.rb
@@ -3,27 +3,24 @@ class Board
 
   attr_reader :cells
   def initialize
-    @cells = {
-      "A1" => Cell.new("A1"),
-      "A2" => Cell.new("A2"),
-      "A3" => Cell.new("A3"),
-      "A4" => Cell.new("A4"),
-      "B1" => Cell.new("B1"),
-      "B2" => Cell.new("B2"),
-      "B3" => Cell.new("B3"),
-      "B4" => Cell.new("B4"),
-      "C1" => Cell.new("C1"),
-      "C2" => Cell.new("C2"),
-      "C3" => Cell.new("C3"),
-      "C4" => Cell.new("C4"),
-      "D1" => Cell.new("D1"),
-      "D2" => Cell.new("D2"),
-      "D3" => Cell.new("D3"),
-      "D4" => Cell.new("D4")
-          }
+    @cells = cell_creator
   end
 
-  # def cell_creator
+  def cell_creator
+    letters = ["A", "B", "C", "D"]
+    numbers = ["1", "2", "3", "4"]
+    cells = {}
+    coordinates = letters.map do |letter|
+    letter = numbers.map do |coordinate|
+          letter + coordinate
+      end
+    end.flatten
+    coordinates.map do |coordinate|
+     cells[coordinate] = Cell.new(coordinate)
+    end
+    cells
+
+  end
 
 
   def valid_coordinate?(coordinate)
