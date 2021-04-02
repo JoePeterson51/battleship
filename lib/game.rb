a99e75c35b06be6e7154961d08b0a5749a4deb63
JoePeterson51 <>
2021-04-02 12:32:35 -0600

    Create computer_add submarine

--- lib/game.rb
+++ lib/game.rb
@@ -26,10 +26,14 @@ class Game
     submarine = Ship.new("Submarine", 2)
     computer_board.cell_creator
     cruiser_cells = @computer_board.cells.keys.combination(3).to_a.shuffle
-    submarine_cells = 
+    submarine_cells = @computer_board.cells.keys.combination(2).to_a.shuffle
     valid_cruiser = cruiser_cells.find do |array|
       @computer_board.valid_placement?(cruiser, array)
     end  
+    valid_submarine = submarine_cells.find do |array|
+      @computer_board.valid_placement?(submarine, array)
+    end 
     @computer_board.place(cruiser, valid_cruiser)
+    @computer_board.place(submarine, valid_submarine)
   end 
 end 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -23,6 +23,17 @@ describe Game do
       game.computer_place
 
       expect(game.computer_board.render(true).include?("S")).to eq(true)
+    end
+    
+    it 'will place both ships' do 
+      game = Game.new 
+      game.computer_place
+      s = game.computer_board.render(true)
+      a = s.split
+      b = a.select do |string|
+        string == "S"
+      end 
+      expect(b.count).to eq(5)
     end 
   end  
 end 
