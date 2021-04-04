4abaca1f22a0389c738c24119877c54b6492ea88
NoahZinter <noah.zinter@gmail.com>
2021-04-04 10:26:20 -0600

    Merge puts refactoring

--- lib/game.rb
+++ lib/game.rb
@@ -6,18 +6,24 @@ class Game
     @game_over = false
   end
 
-  def welcome
+  def welcome_message
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     puts "Welcome to BATTLESHIP!!!!"
     puts "Enter (P) to play. Enter (Q) to quit."
+    puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  end
+
+  def welcome
+    welcome_message
     input = gets.chomp.upcase
-    puts 
+    puts
     if input == "P"
       start
     elsif input == "Q"
       puts "Goodbye!"
     else
       puts "Invalid input"
-      puts 
+      puts
       welcome
     end
   end
@@ -25,7 +31,6 @@ class Game
   def start
     computer_place
     user_place
-
     loop do
       player_shot
       if game_over?
@@ -34,7 +39,6 @@ class Game
         welcome
       break
       end
-
     computer_shot
       if game_over?
         puts "game over"
@@ -45,12 +49,21 @@ class Game
     end
   end
 
+
+  def cruiser_cell_generator
+      @computer_board.cells.keys.combination(3).to_a.shuffle
+  end
+
+  def submarine_cell_generator
+    @computer_board.cells.keys.combination(2).to_a.shuffle
+  end
+
   def computer_place
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
     computer_board.cell_creator
-    cruiser_cells = @computer_board.cells.keys.combination(3).to_a.shuffle
-    submarine_cells = @computer_board.cells.keys.combination(2).to_a.shuffle
+    cruiser_cells = cruiser_cell_generator
+    submarine_cells = submarine_cell_generator
     valid_cruiser = cruiser_cells.find do |array|
       @computer_board.valid_placement?(cruiser, array)
     end
@@ -72,29 +85,29 @@ class Game
     puts "You have a cruiser. It cruises. Place it. It is 3 units long."
     puts "Enter the squares for the Cruiser. They should be three in a row/column."
     puts "NO DIAGONALS!"
-    puts 
-    puts "Enter the first coordinate ->" 
+    puts
+    puts "Enter the first coordinate ->"
   end
 
   def cruiser_place
     cruiser = Ship.new("Cruiser", 3)
     first_coordinate = gets.chomp.upcase!
-    puts 
-    puts "Now enter the second coordinate ->" 
+    puts
+    puts "Now enter the second coordinate ->"
     second_coordinate = gets.chomp.upcase!
-    puts 
+    puts
     puts "Now enter the last coordinate! ->"
     third_coordinate = gets.chomp.upcase!
     puts "----------------------------------"
     cruiser_input = [first_coordinate, second_coordinate, third_coordinate]
       if player_board.valid_placement?(cruiser, cruiser_input) == false
         puts "Those coordinates are not valid!"
-        puts 
+        puts
         user_place
       else
         player_board.place(cruiser, cruiser_input)
         puts player_board.render(true)
-        puts 
+        puts
         puts "^^^ There it is! Your cruuuuuiser."
         puts "----------------------------------"
       end
@@ -103,39 +116,39 @@ class Game
   def user_place
     cruiser_place_greeting
     puts player_board.render
-    puts 
+    puts
     cruiser_coordinate_prompt
     cruiser_place
     user_submarine_place
   end
 
   def submarine_greeting
-    puts 
+    puts
     puts "Now place your submarine. Don't place it on your cruiser."
     puts "It is 2 units long."
-    puts 
+    puts
     puts "Enter your first_coordinate ->"
   end
 
   def submarine_place
     submarine = Ship.new("Submarine", 2)
     submarine_first_coordinate = gets.chomp.upcase!
-    puts 
+    puts
     puts "And now the next coordinate. ->"
     submarine_second_coordinate = gets.chomp.upcase!
     puts "-------------------------------"
     submarine_input = [submarine_first_coordinate, submarine_second_coordinate]
       if player_board.valid_placement?(submarine, submarine_input) == false
         puts "Those coordinates are not valid!"
-        puts 
+        puts
         user_submarine_place
       else
         player_board.place(submarine, submarine_input)
         puts player_board.render(true)
-        puts 
+        puts
         puts "^^^ There it is! Your sub."
         puts "-------------------------------"
-        puts 
+        puts
       end
   end
 
@@ -162,7 +175,7 @@ class Game
 
   def game_over?
     if @player_board.render(true).include?("S") == false
-      puts "Computer has won"
+      puts "Computer has won."
       @game_over = true
     elsif @computer_board.render(true).include?("S") == false
       puts "You have won!!!"
@@ -174,13 +187,13 @@ class Game
   def player_shot
     prompt_shot
     shot = gets.chomp.upcase!
-    if computer_board.valid_coordinate?(shot) && computer_board.cells[shot].fired_upon? == false
+        if computer_board.valid_coordinate?(shot) && computer_board.cells[shot].fired_upon? == false
       computer_board.fire(shot)
     else
       puts "Invalid coordinate. Try again."
       player_shot
     end
-    puts 
+    puts
     show_computer_board
   end
 
@@ -191,12 +204,13 @@ class Game
     end
     unfired.shuffle!
     shot = unfired.shift.coordinate
-    puts 
+    puts
     puts "COMPUTER ----'I shoot at #{shot}!'"
     player_board.fire(shot)
     puts "\n"
+    @computer_board.shot_evaluate
     show_player_board
-    puts 
-    # Add shot evaluation for computer shot. Add shot location for player shot 
+    puts
+    # Add shot evaluation for computer shot. Add shot location for player shot
   end
 end
